334e70f252329b4d22d168e6c553dd0c2767930c
SkyzZ-dev <jordan2officiel@gmail.com>
2022-05-24 17:39:14 -0500

    Created by Maalone

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
 const humbger = document.querySelector(".humbger");
 const humbgermenu = document.querySelector(".humbgermenu");
 
+
 humbger.onclick = function() {
     humbger.classList.toggle("active");
     humbgermenu.classList.toggle("active");
